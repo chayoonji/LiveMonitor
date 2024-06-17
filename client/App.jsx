b1dee51d13b8a9eb4b502decfbde0bf7a52dca0e
cha <zizi2481@gmail.com>
2024-06-17 11:14:36 +0900

    불필요한 주석 삭제

--- client/App.jsx
+++ client/App.jsx
@@ -6,13 +6,11 @@ import Sidebar from './Sidebar';
 import Home from './Home';
 import Team from './team';
 import Guide from './guide';
-import Login from './login'; // 수정된 부분
-import Register from './Register'; // 수정된 부분
-import Reports1 from './reports1'; // 수정된 부분
-import Reports2 from './reports2'; // 수정된 부분
-import PDF from './pdf'; // 수정된 부분
-
-
+import Login from './login';
+import Register from './Register';
+import Reports1 from './reports1';
+import Reports2 from './reports2';
+import PDF from './pdf';
 
 function App() {
   const [openSidebarToggle, setOpenSidebarToggle] = React.useState(false);
@@ -25,17 +23,19 @@ function App() {
     <Router>
       <div className="grid-container">
         <Header OpenSidebar={OpenSidebar} />
-        <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
+        <Sidebar
+          openSidebarToggle={openSidebarToggle}
+          OpenSidebar={OpenSidebar}
+        />
         <Routes>
           <Route path="/guide" element={<Guide />} />
-          <Route path="/login" element={<Login />} /> {/* 수정된 부분 */}
-          <Route path="/register" element={<Register />} /> {/* 수정된 부분 */}
-          <Route path="/reports1" element={<Reports1 />} /> {/* 수정된 부분 */}
-          <Route path="/reports2" element={<Reports2 />} /> {/* 수정된 부분 */}
-          <Route path="/team" element={<Team />} /> {/* 수정된 부분 */}
-          <Route path="/pdf" element={<PDF />} /> {/* 수정된 부분 */}
-      
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reports1" element={<Reports1 />} />
+          <Route path="/reports2" element={<Reports2 />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/pdf" element={<PDF />} />
+          <Route path="/" element={<Guide />} /> {/* 수정된 부분 */}
         </Routes>
       </div>
     </Router>
