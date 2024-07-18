9a46a9e5604615046c5c7cd732ab3016eab7bee4
chayoonji <zizi2481@gmail.com>
2024-07-19 03:43:16 +0900

    로그인사용자구분,로그아웃버튼구현

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import Home from './Home';
+import { AuthProvider } from './Context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
 import Team from './team';
 import Guide from './guide';
-import Login from './login';
+import LoginComponent from './LoginComponent';
 import Register from './Register';
 import Reports1 from './reports1';
 import Reports2 from './reports2';
@@ -20,25 +26,42 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="grid-container">
-        <Header OpenSidebar={OpenSidebar} />
-        <Sidebar
-          openSidebarToggle={openSidebarToggle}
-          OpenSidebar={OpenSidebar}
-        />
-        <Routes>
-          <Route path="/guide" element={<Guide />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/reports1" element={<Reports1 />} />
-          <Route path="/reports2" element={<Reports2 />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/pdf" element={<PDF />} />
-          <Route path="/" element={<Guide />} /> {/* 수정된 부분 */}
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="grid-container">
+          <Header OpenSidebar={OpenSidebar} />
+          <Sidebar
+            openSidebarToggle={openSidebarToggle}
+            OpenSidebar={OpenSidebar}
+          />
+          <Routes>
+            <Route path="/guide" element={<Guide />} />
+            <Route path="/login" element={<LoginComponent />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/pdf" element={<PDF />} />
+            <Route path="/" element={<Navigate to="/guide" />} />{' '}
+            <Route path="/register" element={<Register />} />
+            {/* 기본 경로를 /guide로 리다이렉트 */}
+            <Route
+              path="/reports1"
+              element={
+                <PrivateRoute>
+                  <Reports1 />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/reports2"
+              element={
+                <PrivateRoute>
+                  <Reports2 />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
